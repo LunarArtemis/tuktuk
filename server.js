cf85f1d906dfd69041e8fc2f7c64f583b455c883
Thanakit Chatwichian <thanakit.cha@m.swu.ac.th>
2024-02-28 19:19:33 +0700

    43_change search MySql to MongoDB fix bug

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use('*',(req,res,next)=>{
     next();
 })
 
-//search
+//search endpoint
 app.get('/search/:key', async (req, res) => {
     console.log(req.params.key);
     let data = await User.find(
