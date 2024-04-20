52ad9abd2c8e3329c07cf3c8f6f0cccab991c0a0
Poonyakiat <poonyakiat.lunar@m.swu.ac.th>
2024-04-20 13:36:48 +0700

    Refactor server.js: Add custom 404 page

--- server.js
+++ server.js
@@ -53,7 +53,41 @@ initRoutes(app);
 
 
 app.use('/',(req,res)=>{
-    res.status(404).send('<h1>404 Page not Found</h1>')
+    res.status(404).send(`
+    <!DOCTYPE html>
+    <html lang="en">
+
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>TukTuk</title>
+            <!-- Include Tailwind CSS -->
+            <script src="https://cdn.tailwindcss.com"></script>
+            <link
+                href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.css"
+                rel="stylesheet" />
+            <script
+                src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
+            <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+            <link rel="icon" type="image/x-icon" href="/static/assets/uploads/tuktuk.png">
+
+    </head>
+
+    <body>
+        <section class="bg-white">
+            <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+                <div class="mx-auto max-w-screen-sm text-center">
+                    <h1 class="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-[#8e70b7] ">404</h1>
+                    <p class="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl ">Something's missing.</p>
+                    <p class="mb-4 text-lg font-light text-gray-500">Sorry, we can't find that page. You'll find lots to explore on the home page. </p>
+                    <a href="/" class="inline-flex text-white bg-[#8e70b7] hover:bg-[#684e89] focus:ring-4 focus:outline-none focus:ring-[#d3cbe7] font-medium rounded-lg text-sm px-5 py-2.5 text-center my-4">Back to Homepage</a>
+                </div>   
+            </div>
+        </section>
+    </body>
+    </html>
+    `)
 })
 let port = 3000;
 app.listen(port, () => {
