21b3128331391fe16c13fc33211e135f51749197
Thanakit Chatwichian <thanakit.cha@m.swu.ac.th>
2024-04-21 21:43:17 +0700

    80:fix UI/add authur

--- src/controllers/pinController.js
+++ src/controllers/pinController.js
@@ -1,8 +1,16 @@
 const Image = require('../models/images.model.js');
+const User = require('../models/User.js');
 
 const pin = async(req, res) => {
-    let data = await Image.findById(req.params.id);
-    res.render('pin', { data: data });
+    const userId = req.session.userId;
+    const data = await Image.findById(req.params.id);
+    const user = await User.findById(data.uploaded_by);
+
+    if (user) {
+        res.render('pin', { data: data , User: user.username});
+    } else {
+        res.status(404).send('User not found');
+    }
 };
 
 module.exports = pin;
